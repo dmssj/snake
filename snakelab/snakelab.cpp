7c8215bf20e9ecc3374f001ecb6f0e2ae7e9a1f3
dmssj <letrey19@gmail.com>
2024-12-18 01:46:52 +0700

    коммент

--- snakelab/snakelab.cpp
+++ snakelab/snakelab.cpp
@@ -1,6 +1,6 @@
 #include <iostream> //змейка
-#include <conio.h>
-#include <thread>
+#include <conio.h>  //консольные функц
+#include <thread> 
 #include <chrono>
 using namespace std;
 
@@ -19,9 +19,9 @@ int main() {
 
     while (!gameOver) {
         system("cls");
-        for (int i = 0; i < width + 2; i++) cout << "_"; cout << endl;
+        for (int i = 0; i < width + 2; i++) cout << "_"; cout << endl; //верх
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+            for (int j = 0; j < width; j++) { //лево
                 if (j == 0) cout << "|";
                 if (i == y && j == x) cout << "o";
                 else if (i == fruity && j == fruitx) cout << "$";
@@ -31,13 +31,13 @@ int main() {
                         if (tailx[k] == j && taily[k] == i) { cout << "o"; print = true; }
                     if (!print) cout << " ";
                 }
-                if (j == width - 1) cout << "|";
+                if (j == width - 1) cout << "|"; //право
             }
             cout << endl;
         }
-        for (int i = 0; i < width + 2; i++) cout << "_"; cout << "\nСчет: " << score << endl;
+        for (int i = 0; i < width + 2; i++) cout << "_"; cout << "\nСчет: " << score << endl; //низ
 
-        if (_kbhit()) {
+        if (_kbhit()) { //управ змейкой
             switch (_getch()) {
             case 'a': dir = Left; break;
             case 'd': dir = Right; break;
@@ -47,7 +47,7 @@ int main() {
             }
         }
 
-        int prevX = tailx[0], prevY = taily[0], prev2X, prev2Y;
+        int prevX = tailx[0], prevY = taily[0], prev2X, prev2Y; //движ змейки
         tailx[0] = x; taily[0] = y;
         for (int i = 1; i < n_tail; i++) {
             prev2X = tailx[i]; prev2Y = taily[i];
@@ -60,10 +60,13 @@ int main() {
         case Up: y--; break;
         case Down: y++; break;
         }
-        if (x < 0 || x >= width || y < 0 || y >= height) gameOver = true;
+        if (x < 0 || x >= width || y < 0 || y >= height) gameOver = true; //столкновение
         for (int i = 0; i < n_tail; i++) if (tailx[i] == x && taily[i] == y) gameOver = true;
-        if (x == fruitx && y == fruity) {
-            score += 10; fruitx = rand() % width; fruity = rand() % height; n_tail++;
+        if (x == fruitx && y == fruity) { //еда
+            score += 10; 
+            fruitx = rand() % width; 
+            fruity = rand() % height; 
+            n_tail++;
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
